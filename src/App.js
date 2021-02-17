7b47e4b722cb13a42683cc3c95e7f533d70bc9ae
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2021-02-17 00:13:20 -0500

    Test functionality of tasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react'
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id:1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2.30pm',
+            reminder: true,
+        },
+        {
+            id:2,
+            text: 'Meeting at school',
+            day: 'Feb 6th at 2.30pm',
+            reminder: true,
+        },
+        {
+            id:3,
+            text: 'Food shopping',
+            day: 'Feb 5th at 2.30pm',
+            reminder: false,
+        }
+    ])
+
+    // Delete task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    // toggle reminder
+    const toggleReminder = (id) =>{
+        console.log(id)
+    }
+
     return (
         <div className='container'>
             <Header />
+            <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
         </div>
     )
 }
